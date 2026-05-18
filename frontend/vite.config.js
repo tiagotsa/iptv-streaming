57386dcdf3af64b3cf101485450f0ea4e6e74fe1
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2026-05-18 20:37:50 -0300

    vite.config reparo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,28 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({ include: '**/*.{js,jsx,ts,tsx}' })],
-  esbuild: {
-    loader: 'jsx',
-    include: /\.[jt]sx?$/,
-    jsx: 'automatic',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-        '.ts': 'tsx',
-      },
-    },
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
   },
   server: {
     port: 3000,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-      },
-    },
+    strictPort: false,
   },
 });
